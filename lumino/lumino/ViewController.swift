6b0df926cddccbdd3f3a181a1592ab8d213968ae
t-kashima <takumi.kashima@nanapi.co.jp>
2016-06-25 10:59:27 +0900

    add xcode project

---
+++ lumino/lumino/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lumino
+//
+//  Created by takumi.kashima on 2016/06/25.
+//  Copyright © 2016年 UNUUU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
